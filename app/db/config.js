3b3f496bc667645d7ed0cc40def7c7487dbd8245
Sandesh Mankar <mankarsandesh11@gmail.com>
2023-02-12 17:35:31 +0700

    Change : ENV db port add

--- app/db/config.js
+++ app/db/config.js
@@ -1,31 +1,39 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
-    dialectOptions: {
-        //useUTC: false,
-        dateStrings: true,
-        typeCast: function (field, next) { // for reading from database
-            if (field.type === 'DATETIME') {
-                return field.string();
-            }
-            return next();
-        }
-    },
-    timezone: process.env.DB_TIMEZONE
-});
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USERNAME,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOSTNAME,
+		port: process.env.DB_PORT,
+		dialect: 'mysql',
+		dialectOptions: {
+			//useUTC: false,
+			dateStrings: true,
+			typeCast: function (field, next) {
+				// for reading from database
+				if (field.type === 'DATETIME') {
+					return field.string()
+				}
+				return next()
+			},
+		},
+		timezone: process.env.DB_TIMEZONE,
+	}
+)
 
 //Check if the database is connected successfully
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection established successfully');
-    })
-    .catch((error) => {
-        console.log(error);
-});
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection established successfully')
+	})
+	.catch((error) => {
+		console.log(error)
+	})
 
 //Create db table if it does not exist
-sequelize.sync();
+sequelize.sync()
 
-module.exports = sequelize;
+module.exports = sequelize
