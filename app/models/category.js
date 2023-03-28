b8671ffcf18f948d88687ee868c19200ebf5f2e6
Sandesh Mankar <mankarsandesh11@gmail.com>
2023-03-28 21:49:36 +0700

    feat : add new column in category listing image url

--- app/models/category.js
+++ app/models/category.js
@@ -1,46 +1,50 @@
-const DataTypes = require("sequelize");
-const db = require("../db/config");
+const DataTypes = require('sequelize')
+const db = require('../db/config')
 
 const Category = db.define(
-  "category",
-  {
-    categoryID: {
-      type: DataTypes.BIGINT(20).UNSIGNED,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    categoryName: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true,
-    },
-    categoryDescription: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.literal("CURRENT_TIMESTAMP()"),
-      field: "created_at",
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.literal("CURRENT_TIMESTAMP()"),
-      field: "updated_at",
-    },
-    deleted_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-  },
-  {
-    freezeTableName: true,
-    tableName: "category",
-  }
-);
+	'category',
+	{
+		categoryID: {
+			type: DataTypes.BIGINT(20).UNSIGNED,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		categoryName: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: true,
+		},
+		categoryDescription: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: true,
+		},
+		categoryImageUrl: {
+			type: DataTypes.STRING(255),
+			default: null,
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.literal('CURRENT_TIMESTAMP()'),
+			field: 'created_at',
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.literal('CURRENT_TIMESTAMP()'),
+			field: 'updated_at',
+		},
+		deleted_at: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+	},
+	{
+		freezeTableName: true,
+		tableName: 'category',
+	}
+)
 
-module.exports = Category;
+module.exports = Category
